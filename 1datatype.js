8d4bce01a2953f26ccb90934d101aaa01d4e7a2d
aakashdeveloper <ahanda205@gmail.com>
2021-06-10 09:54:44 +0530

    datatype

---
+++ 1datatype.js
@@ -0,0 +1,102 @@
+///Datatypes
+
+string = "adfdferf"  'hi'  "434" '44'  'ef45' 'true'
+number = 434 4  44564565756 457.5574 .45575 5475674568.5657876
+Boolean = true false
+
+null
+undefined
+BigInt
+Symbol
+
+////es5/////
+var a = "hi"
+var b = 10
+var c = true
+
+a
+"hi"
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "10"
+typeof(d)
+"string"
+var e = 10.343
+typeof(e)
+"number"
+
+var f='hello'
+typeof(f)
+"string"
+////////////
+
+var a =10
+var b=20
+a+b (add)
+30
+a-b
+-10
+a*b
+200
+b/a
+2
+
+4%2
+0
+
+2%4
+2
+3%4
+3
+
+
+var a = "Hi"
+var b ="JavaScript"
+a+b (concat)
+"HiJavaScript"
+var c = 10
+a+c
+"Hi10"
+a-b
+NaN (not a number)
+a/b
+NaN
+a*b
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+
+10+"20"+30
+"10"+20+30
+10+20+"30"
+
+
+10+"20"+30
+"1020"+30
+>"102030"
+
+"10"+20+30
+"1020"+30
+>"102030"
+
+10+20+"30"
+30+"30"
+>"3030"
+
+
+10+"20"+30-1
+> 102029
+
+"10"+20+30-1
+> 102029
+
+10+20+"30"-1
+> 3029
