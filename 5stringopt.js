9ea8f312bada37d80f1aa5ceacce4e6e1ff048cd
aakashdeveloper <ahanda205@gmail.com>
2021-06-16 09:56:07 +0530

    maths

--- 5stringopt.js
+++ 5stringopt.js
@@ -109,3 +109,50 @@ city.slice(0,-1)
 ///// first -ve and second +ve > no output
 city.slice(-1,3)
 ""
+//
+var a = "i am learning javascript"
+a.replace('javascript','JS')
+"i am learning JS"
+var a = "javascript i am learning javascript"
+undefined
+a.replace('javascript','js')
+"js i am learning javascript"
+a.replace(/javascript/,'js')
+"js i am learning javascript"
+a.replace(/javascript/g,'js')
+"js i am learning js"
+
+var name = "   Bhumika .  "
+undefined
+name.replace(/ /g,'')
+"Bhumika."
+name.replace(/ /g,'-')
+"---Bhumika-.--"
+
+
+var url = "https://github.com/Aakashdeveloper/April_React_Evng";
+url.split('/')
+(5) ["https:", "", "github.com", "Aakashdeveloper", "April_React_Evng"]
+var out = url.split('/')
+undefined
+out[out.length-1]
+"April_React_Evng"
+
+var a = "javascript i am learning javascript"
+undefined
+a.split(' ')
+(5) ["javascript", "i", "am", "learning", "javascript"]
+
+var a = "javascript"
+a.split('')
+(10) ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"]
+
+var out = a.split('')
+out
+(10) ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"]
+
+out.toString()
+
+"j,a,v,a,s,c,r,i,p,t"
+out.toString().replace(/,/g,'')
+"javascript"
---
+++ 6stringLiterals.js
@@ -0,0 +1,15 @@
+//es5
+var name = "Avengers"
+var rating = 4.5
+var type = "action"
+var ind = "Hollywood"
+
+var out = "The "+name+" is an "+type+" movie, with rating of "+rating+" and from ind "+ind+".";
+
+console.log("this is es5",out)
+"The Avengers is an action movie, with rating of 4.5 and from ind Hollywood."
+
+//es6
+var out1 = `The ${name} is an ${type} movie, with rating of ${rating} and from ind ${ind}.`
+"The Avengers is an action movie, with rating of 4.5 and from ind Hollywood."
+console.log("this is es6",out1)
