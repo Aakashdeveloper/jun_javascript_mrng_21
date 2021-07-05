7937f0330b2cb0192f33ab76a1334153c3438f0a
aakashdeveloper <ahanda205@gmail.com>
2021-07-05 10:04:59 +0530

    calc

--- 28prototypeEs6.js
+++ 28prototypeEs6.js
@@ -3,16 +3,19 @@ class geo{
         this.lat=43.55;
         this.long=32.11;
     }
+
+    abc =(a,b) => {
+        this.lat 
+    }
 }
 
 class language extends geo{
     constructor(name,city,country){
-        super(lat,long)
+        super()
 
         this.name = name;
         this.country = country;
         this.city = city;
-        this.lat = lat
     }
 }
 
---
+++ 30calc.js/calc.js
@@ -0,0 +1,39 @@
+function calc(opt){
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out;
+    // here we are checking input value
+    if(a.length>0 || b.length>0){
+        if(isNaN(a) || isNaN(b)){
+            out="Enter valid input" 
+        }else{
+            // if user click on add
+            if(opt== "Add"){
+                out = Number(a)+Number(b);
+            // if user click on sub
+            }else{
+                out = Number(a)-Number(b);
+            }
+        } 
+    }else{
+        out="Please Enter value"  
+    }
+   
+    document.getElementById('output').innerText=out
+}
+
+/*
+function add(){
+                var a = document.getElementById('first').value;
+                var b = document.getElementById('second').value;
+                var out = Number(a)+Number(b);
+                document.getElementById('output').innerText=out
+            }
+
+            function sub(){
+                var a = document.getElementById('first').value;
+                var b = document.getElementById('second').value;
+                var out = Number(a)-Number(b);
+                document.getElementById('output').innerText=out
+            }
+*/
